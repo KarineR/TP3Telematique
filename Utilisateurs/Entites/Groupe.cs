65ce58371a8674866e90da366848137d31bef89a
Fanny St-Laurent <fanny.stla@gmail.com>
2012-04-09 21:03:36 -0400

    [Fanny] Ajout de la page de création de groupe (Création fonctionnelle, accessible directement par l'url)

--- Utilisateurs/Entites/Groupe.cs
+++ Utilisateurs/Entites/Groupe.cs
@@ -7,11 +7,22 @@ namespace Entites.Utilisateurs
 {
   public class Groupe
   {
-    public Groupe() { }
+    public const string FICHIER_LISTE_GROUPE = "groupes.xml";
 
-    public int Id { get; set; }
-    public List<Utilisateur> Pairs { get; set; }
-    public int AdministrateurId { get; set; }
+    public Groupe() { }
+    public Groupe(string nom, List<Guid> pairs, Guid AdminId)
+    {
+      this.Id = Guid.NewGuid();
+      this.Nom = nom;
+      this.Pairs = pairs;
+      this.AdministrateurId = AdminId;
+      this.Fichiers = new List<Fichier>();
+    }
+
+    public Guid Id { get; set; }
+    public string Nom {get; set; }
+    public List<Guid> Pairs { get; set; }
+    public Guid AdministrateurId { get; set; }
     public List<Fichier> Fichiers { get; set; }
   }
 }
